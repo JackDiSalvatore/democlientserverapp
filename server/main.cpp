57f36ca972055b69ebfd151a2a0c5ab1fc74cf95
jackdisalvatore <jackp.disalvatore@gmail.com>
2023-11-08 11:33:28 -0600

    allowing multiple clients using std::thread

--- server/main.cpp
+++ server/main.cpp
@@ -6,17 +6,16 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <string>
+#include <thread>
+#include <vector>
 
 class TCPServer {
 public:
-    TCPServer() : listening(-1), clientSocket(-1) {}
+    TCPServer() : listening(-1) {}
 
     ~TCPServer() {
         if (listening != -1) {
             close(listening);
-        }
-        if (clientSocket != -1) {
-            close(clientSocket);
         }
     }
 
@@ -35,11 +34,10 @@ public:
         struct sockaddr_in hint;
         hint.sin_family = AF_INET;
         hint.sin_port = htons(54000);
-        // inet_pton(AF_INET, "172.28.0.2", &hint.sin_addr);
         inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr);
 
         // Binding Information
-        if (-1 == bind(listening, (struct sockaddr *) &hint, sizeof(hint))) {
+        if (-1 == bind(listening, (struct sockaddr*) &hint, sizeof(hint))) {
             std::cerr << "Cannot bind to socket";
             return -2;
         }
@@ -50,74 +48,60 @@ public:
             return -3;
         }
 
-        // Accept an incoming call
-        sockaddr_in client;
-        socklen_t clientSize = sizeof(client);
-        char host[NI_MAXHOST];
-        char service[NI_MAXSERV];
-
-        // This is our new "communication" socket
-        clientSocket = accept(listening, (sockaddr*) &client, &clientSize);
-
-        if (-1 == clientSocket) {
-            std::cerr << "Problem accepting client";
-            return -4;
-        }
-
-        // Close the listening socket
-        close(listening);
-
-        // Clean up memory
-        memset(host, 0, NI_MAXHOST);
-        memset(service, 0, NI_MAXSERV);
-
-        // Get host machine name
-        int result = getnameinfo((sockaddr *) &client, clientSize, host, NI_MAXHOST, service, NI_MAXSERV, 0);
-
-        if (result) {
-            std::cout << host << " connected on " << service << std::endl;
-        } else {
-            inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
-            std::cout << host << " connected on " << ntohs(client.sin_port) << std::endl;
-        }
-
         return 0;
     }
 
     void runServer() {
-        // Receive messages
+        while (true) {
+            // Accept an incoming call
+            sockaddr_in client;
+            socklen_t clientSize = sizeof(client);
+
+            // This is our new "communication" socket
+            int clientSocket = accept(listening, (sockaddr*) &client, &clientSize);
+
+            if (-1 == clientSocket) {
+                std::cerr << "Problem accepting client";
+                continue;
+            }
+
+            // Use a separate thread to handle this client
+            std::thread clientThread(&TCPServer::handleClient, this, clientSocket);
+            clientThread.detach(); // Detach the thread, allowing it to run independently
+        }
+    }
+
+    void handleClient(int clientSocket) {
         char buf[4096];
+
         while (true) {
             // Clear buffer
             memset(buf, 0, 4096);
 
             // Wait for messages
-            int bytesRecv = recv(clientSocket, buf, 4096, 0);   // "0" for no flags
+            int bytesRecv = recv(clientSocket, buf, 4096, 0);
 
             if (-1 == bytesRecv) {
                 std::cerr << "There was a connection issue" << std::endl;
+                break;
             } else if (0 == bytesRecv) {
-                // std::cerr << "The client disconnected" << std::endl;
+                // The client disconnected
                 break;
             } else {
                 // Display message
                 std::cout << "Received: " << std::string(buf, 0, bytesRecv) << std::endl;
 
                 // Resend "echo" message
-                send(clientSocket, buf, bytesRecv + 1, 0);  // "0" for no flags
+                send(clientSocket, buf, bytesRecv + 1, 0); // "0" for no flags
             }
         }
 
         // Close client socket
         close(clientSocket);
-
-        // Done
-        std::cout << "Done!" << std::endl;
     }
 
 private:
     int listening;
-    int clientSocket;
 };
 
 int main() {
